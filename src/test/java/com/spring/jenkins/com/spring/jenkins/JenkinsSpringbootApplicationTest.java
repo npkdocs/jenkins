6611c7c79a05cd6c6f700713401b0cfc8aee129a
npkdocs <npkdocs@gmail.com>
2022-10-30 19:40:21 +0530

    added SpringBootTest

--- src/test/java/com/spring/jenkins/com/spring/jenkins/JenkinsSpringbootApplicationTest.java
+++ src/test/java/com/spring/jenkins/com/spring/jenkins/JenkinsSpringbootApplicationTest.java
@@ -8,11 +8,13 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * @author npk61
  *
  */
+@SpringBootTest(classes = JenkinsSpringbootApplication.class)
 class JenkinsSpringbootApplicationTest {
 
 	static Logger logger = LoggerFactory.getLogger(JenkinsSpringbootApplicationTest.class);
